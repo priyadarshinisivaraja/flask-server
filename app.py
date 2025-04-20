998c6c00a2e42f6206a7496bac1cf646542bf9d7
priyadarshinisivaraja <photonidol@gmail.com>
2025-04-20 11:16:32 +0530

    Initial Commit

---
+++ app.py
@@ -0,0 +1,12 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return 'Hello, this is your public Flask server!'
+
+if __name__ == '__main__':
+    import os
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
